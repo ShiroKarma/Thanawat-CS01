342cf138dd12e791e3a9bfcfbd443857ae75df2a
ShiroKarma <able14062002@gmail.com>
2021-08-20 09:05:56 +0700

    more more

---
+++ CS01-13.py
@@ -0,0 +1,9 @@
+i = 1
+num = 1
+while (i == 1):
+    x = int(input("Score :"))
+    if (x == -1):
+        break
+    else:
+        print("number :", num ,"score = ", x)
+        num += 1
---
+++ CS01-14.py
@@ -0,0 +1,5 @@
+List = [5,10,15,20,25,50,20]
+for i in range(len(List)):
+    if (List[i] == 20) :
+        List[i] = 200
+print(List)
